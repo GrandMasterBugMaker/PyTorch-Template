481b2bfc194bbdd6794a9e91a2cb3eb3b6b9347b
Bailingnan <lingnanbai@gmail.com>
2020-04-30 19:08:36 +0800

    Delete __init__.py

--- Pytorch-Template/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*- 
-"""
-# @Author:      Bai Lingnan
-# @Project:     Pytorch-Template
-# @Filename:    __init__.py
-# @Time:        2020/3/12 09:34
-"""
-
